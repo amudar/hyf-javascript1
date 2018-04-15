f66a5d0c3cb0aa4223283df10e04ad0fe5714b11
matfeh <matfeh@outlook.com>
2018-04-15 15:45:45 +0200

    removed startWithHello

---
+++ hello.js
@@ -0,0 +1,14 @@
+console.log('Hello World');
+const myName = 'Jim';
+let age = 63;
+age = 64;
+const isMale = true;
+console.log(myName, age, isMale);
+const months = ['January', 'February', 'December'];
+console.log(months[1]);
+
+console.log(typeof myName);
+const hello = 'Hello world!'
+const part = hello.slice(0, 5);
+const upper = hello.toUpperCase();
+console.log(part, upper);
