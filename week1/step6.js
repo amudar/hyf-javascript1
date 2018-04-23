5af59928f6a7d03b443e9d923ad6609506001cde
amudar <matfeh@outlook.com>
2018-04-23 20:34:23 +0200

    Update step6.js

--- week1/step6.js
+++ week1/step6.js
@@ -1,8 +1,8 @@
 'use strict';
 
-let wildBirds = [];
-console.log('the value of wildBirds could be: (eagle, falcon and owl)');
-console.log(wildBirds);
+let Pets = [];
+console.log('the value of Pets could be: null');
+console.log(Pets);
 let myPets = [ 'cat', 'dog', 'rabbit' ];
 console.log(myPets);
 myPets.push('baby pig');
